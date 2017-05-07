87c22177733431e7da671afc5166cc4e48feca1c
abigailyuan <abigail_yuan@hotmail.com>
2017-05-07 11:37:43 +1000

    read file to dict

--- langid/train/printData.py
+++ langid/train/printData.py
@@ -1,9 +1,15 @@
 import json
+from collections import defaultdict as dd
 from pprint import pprint
+
 
 import collections
 
 with open('dev.json') as json_data:
-	data = json.load(json_data)
-	print(data)
-
+    data = json.load(json_data)
+    #print(data)
+    langDict = dd(int)
+    for item in data:
+        lang = item.get("lang")
+        langDict[lang] += 1
+    print(langDict) 
